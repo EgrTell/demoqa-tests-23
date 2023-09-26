5b997c17e84baadd716c9a8f692fe678e04e2018
Egor Tellin <egor21chuv@yandex.ru>
2023-09-26 17:35:54 +0300

    added build.gradle

---
+++ src/test/java/JUnitExamples.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.*;
+
+public class JUnitExamples {
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Here is beforeAll");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("    Here is beforeEach");
+    }
+    @AfterAll
+    static void AfterAll() {
+        System.out.println("Here is AfterAll");
+    }
+    @AfterEach
+    void AfterEach() {
+        System.out.println("    Here is AfterEach");
+    }
+    @Test
+    void firstTest() {
+        System.out.println("        Here is firstTest");
+    }
+    @Test
+    void secondTest() {
+        System.out.println("        Here is secondTest");
+    }
+}
