f11b16b4f5718e9e52385a5a328462d3cfd28399
Egor Tellin <egor21chuv@yandex.ru>
2023-10-02 16:10:56 +0300

    added PracticForm

--- src/test/java/exampls/tests/PracticFormTests.java
+++ src/test/java/exampls/tests/PracticFormTests.java
@@ -1,10 +1,12 @@
+package exampls.tests;
+
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class PracticForm {
+public class PracticFormTests {
     @Test
     void fillPracticeForm () {
         Configuration.holdBrowserOpen=true;
@@ -14,11 +16,12 @@ public class PracticForm {
         $("[id=userEmail]").setValue("egor21chuv@yandex.ru");
         $("[class=custom-control-label]").click();
         $("[id=userNumber]").setValue("89656876462");
+        $("[id=currentAddress]").setValue("Some Address 1");
         $("[id=dateOfBirthInput]").click();
         $("[id=dateOfBirthInput]").setValue("\b\b\b\b\b\b\b\b\b\b");
         $("[id=dateOfBirthInput]").setValue("19 Apr 1999");
 
-         $("[class=subjects-auto-complete__placeholder]").click();
+        // $("[class=subjects-auto-complete__placeholder]").click();
         //  $("[id=subjects-auto-complete__value-container]").setValue("m");
        //  $("[id=subjects-auto-complete__value-container]").pressEnter();
 
--- src/test/java/exampls/tests/TextBoxTests.java
+++ src/test/java/exampls/tests/TextBoxTests.java
@@ -1,3 +1,4 @@
+package exampls.tests;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
