f1aaceb7b0509117f1a27037b0491bcb5589bd85
Egor Tellin <egor21chuv@yandex.ru>
2023-10-03 17:50:33 +0300

    added hobbies

--- src/test/java/exampls/tests/PracticFormTests.java
+++ src/test/java/exampls/tests/PracticFormTests.java
@@ -3,6 +3,7 @@ package exampls.tests;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -11,19 +12,35 @@ public class PracticFormTests {
     void fillPracticeForm () {
         Configuration.holdBrowserOpen=true;
         open("https://demoqa.com/automation-practice-form");
+
         $("[id=firstName]").setValue("Egor");
         $("[id=lastName]").setValue("Tellin");
         $("[id=userEmail]").setValue("egor21chuv@yandex.ru");
-        $("[class=custom-control-label]").click();
-        $("[id=userNumber]").setValue("89656876462");
         $("[id=currentAddress]").setValue("Some Address 1");
-        $("[id=dateOfBirthInput]").click();
-        $("[id=dateOfBirthInput]").setValue("\b\b\b\b\b\b\b\b\b\b");
-        $("[id=dateOfBirthInput]").setValue("19 Apr 1999");
+        $("[id=userNumber]").setValue("89656876462");
 
-        // $("[class=subjects-auto-complete__placeholder]").click();
-        //  $("[id=subjects-auto-complete__value-container]").setValue("m");
-       //  $("[id=subjects-auto-complete__value-container]").pressEnter();
+        //$("[class=custom-control-label]").click(); // only for male
+//        $("label[for=gender-radio-3]").click(); //good
+        $("#genterWrapper").$(byText("Other")).click();// best
+
+        $("[id=dateOfBirthInput]").click();
+        $("[class=react-datepicker__month-select]").selectOption("April");
+        $("[class=react-datepicker__year-select]").selectOption("1999");
+        $(".react-datepicker__day--019").click();
+
+        $("[id=subjectsInput]").setValue("Maths").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+
+
+
+
+//        $("[id=dateOfBirthInput]").click();$("[id=dateOfBirthInput]").setValue("\b\b\b\b\b\b\b\b\b\b");
+//        $("[id=dateOfBirthInput]").setValue("19 Apr 1999");
+
+//         $("[class=subjects-auto-complete__placeholder]").click();
+//          $("[id=subjects-auto-complete__value-container]").setValue("m");
+//         $("[id=subjects-auto-complete__value-container]").pressEnter();
 
 
     }
